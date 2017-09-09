ebf6d5743d9c8c0f69b6f3850269bb243b14f16c
waditya <waditya02@gmail.com>
2017-09-09 09:53:32 -0500

    Update 01_Arrays_DS.py

--- 01_Arrays_DS.py
+++ 01_Arrays_DS.py
@@ -8,5 +8,7 @@ arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
 
 #! Code starts here
 
+#Reverse the order of display and use " " as the value for argument 'end' of print function
+
 for i in range(n-1,-1,-1):
     print(arr[i], end = " ")
