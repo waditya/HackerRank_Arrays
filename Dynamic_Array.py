3d531dadcc479e371be4bd9ae3df13d57cafa7e1
waditya <waditya02@gmail.com>
2017-09-08 19:15:43 -0500

    Update Dynamic_Array.py
    Code commit

--- Dynamic_Array.py
+++ Dynamic_Array.py
@@ -1,1 +1,15 @@
+## Dynamic Array
+#!/bin/python3
 
+n,m = map(int, input().split())
+lastAnswer = 0
+seqList = [[] for _ in range(n)]
+for _ in range(m):
+    case,x,y = [int(temp) for temp in input().split()]
+    if case == 1:
+        seq = (x ^ lastAnswer) % n
+        seqList[seq].append(y)
+    if case == 2:
+        seq = (x ^ lastAnswer) % n
+        lastAnswer = seqList[seq][y % len(seqList[seq])]
+        print(lastAnswer)
