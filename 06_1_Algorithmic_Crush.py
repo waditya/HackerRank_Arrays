a90d62e8825553ca7226c8b55056e011c109b7a5
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-09-10 10:53:44 -0500

    Create 06_1_Algorithmic_Crush.py

---
+++ 06_1_Algorithmic_Crush.py
@@ -0,0 +1,23 @@
+## Accept the first line of the input
+no_of_elememts_in_array, no_of_operations = [int(n) for n in input().split(" ")]
+
+##print(no_of_elememts_in_array) ## -- DEBUG n = no_of_elememts_in_array
+##print(no_of_operations) ## -- no_of_operations
+
+
+list = [0]*(no_of_elememts_in_array+1)
+##print(list) ## -- DEBUG - Check if all the elememts in the list are marked zero
+
+## Read the operations using for loop for 'no_of_operations' times
+for _ in range(no_of_operations):
+    x, y, incr = [int(n) for n in input().split(" ")]
+    ## Increment the element in lowerbound index
+    list[x-1] += incr
+    
+    if((y)<=len(list)): 
+        list[y] -= incr;
+max = x = 0
+for i in list:
+    x=x+i;
+    if(max<x): max=x;
+print(max)
